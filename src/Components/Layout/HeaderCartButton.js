ca92be321c113fad376d81390e159802943e92fd
melika760 <72795928+melika760@users.noreply.github.com>
2023-06-24 17:13:13 +0330

    Update HeaderCartButton.js

--- src/Components/Layout/HeaderCartButton.js
+++ src/Components/Layout/HeaderCartButton.js
@@ -1,14 +1,19 @@
-
+import React,{useContext} from "react";
 import CartIcon from "../Cart/CartIcon";
 import styles from "./HeaderCartButton.module.css";
+import CartContext from "../../Store/cart-context";
 export default function HeaderCartButton(props){
+    const ctx = useContext(CartContext)
+    const BageNumber = ctx.items.reduce((CurrNum,item)=>{
+        return CurrNum + item.amount
+    },0)
     return(
         <button className={styles.button} onClick={props.onShow}>
             <span className={styles.icon}>
                 <CartIcon/>
             </span>
             <span>Your Card</span>
-            <span className={styles.badge}>3</span>
+            <span className={styles.badge}>{BageNumber}</span>
             
         </button>
      
